fa1b279491b68e4c4835dc19f28f01289622216c
PriyanshuRj <priyanshurajput0071109@gmail.com>
2022-06-01 13:22:42 +0530

    Updated Hero Ui and added Leader section

--- src/components/home/hero.js
+++ src/components/home/hero.js
@@ -2,27 +2,13 @@ import { Fragment } from 'react'
 import { Popover, Transition } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
 
-const navigation = [
-  { name: 'Product', href: '/' },
-  { name: 'Features', href: '/' },
-  { name: 'Marketplace', href: '/' },
-  { name: 'Company', href: '/' },
-]
 
 export default function Hero() {
   return (
     <div className="relative bg-white overflow-hidden">
       <div className=" ">
         <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-          {/* <svg
-            className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
-            fill="currentColor"
-            viewBox="0 0 100 100"
-            preserveAspectRatio="none"
-            aria-hidden="true"
-          >
-            <polygon points="50,0 100,0 50,100 0,100" />
-          </svg> */}
+   
 
           <Popover>
             <div className="relative pt-6 sm:px-6 lg:px-8">
@@ -45,11 +31,7 @@ export default function Hero() {
                 <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
                   <div className="px-5 pt-4 flex items-center justify-between">
                     <div>
-                      <img
-                        className="h-8 w-auto"
-                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                        alt=""
-                      />
+                      
                     </div>
                     <div className="-mr-2">
                       <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -85,14 +67,7 @@ export default function Hero() {
                     Schedule GD
                   </a>
                 </div>
-                {/* <div className="mt-3 sm:mt-0 sm:ml-3">
-                  <a
-                    href="/"
-                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
-                  >
-                    Live demo
-                  </a>
-                </div> */}
+ 
                
               </div><h2 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-3xl md:text-4xl mt-8">
               Join Group Discussions for Free</h2>
@@ -105,7 +80,7 @@ export default function Hero() {
       </div>
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <img
-          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+          className=" w-full object-cover lg:w-full lg:h-full"
           src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
           alt=""
         />
